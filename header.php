544f77c3d72038f846e0ea7892264e706ab28c5e
Rodrigo Santos <rosonline.mail@gmail.com>
2022-06-11 09:51:27 -0300

    Menu do Site (antes da alteração repentina)
    Versão do header.php antes da mudança de lay-out durante a aula de Programação WEB do dia 10/06/2022.

--- header.php
+++ header.php
@@ -8,24 +8,75 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	<!-- JavaScript Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+	<!-- Ícones do BootStrap -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+	<!-- CSS Local -->
 	<link rel="stylesheet" href="css/styles.css">
 	<title>Projeto Final Tido :: Temporário</title>
 </head>
 <body>
 	<!-- Navbar -->
 	<nav class="barra_menu">
-    <a href="#">Empresa</a>
+		<div class="logo">
+			<a href="#">Empresa</a>
+		</div>
+		<div class="lista-menu">
+			<ul>
 
-    <ul>
+       		<li>Home</li>
+       		<li>Sobre a Empresa</li>
+       		<li>Serviços</li>
+       		<li>Portfólio</li>
+       		<li>Contato</li>
+   		</ul>
+		</div>
+		<div class="Login">
+			<i class="bi bi-person-circle" data-bs-toggle="modal" data-bs-target="#myModal">
+   				<div class="container mt-3">
+   					
+   				</div>
+				<!-- The Modal -->
+				<div class="modal" id="myModal">
+  					<div class="modal-dialog">
+    				<div class="modal-content">
 
-       <li>Home</li>
-       <li>Sobre a Empresa</li>
-       <li>Serviços</li>
-       <li>Portfólio</li>
-       <li>Contato</li>
-   </ul>
-   <a href="#">login</a>
+      				<!-- Modal Header -->
+      				<div class="modal-header">
+        				<h4 class="modal-title">Login</h4>
+        				<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        			</div>
+
+     			 <!-- Modal body -->
+      			<div class="modal-body">
+         			<form action="/action_page.php" class="was-validated">
+  				<div class="mb-3 mt-3">
+    			<label for="uname" class="form-label">Nome de Usuário:</label>
+    				<input type="text" class="form-control" id="uname" placeholder="Insira o nome de usuário" name="uname" required>
+    			<div class="valid-feedback">Valid.</div>
+    			<div class="invalid-feedback">Insira o nome de usuário corretamente.</div>
+  				</div>
+  				<div class="mb-3">
+    				<label for="pwd" class="form-label">Senha:</label>
+    				<input type="password" class="form-control" id="pwd" placeholder="Insira a Senha" name="pswd" required>
+   					 <div class="valid-feedback">Valid.</div>
+    				<div class="invalid-feedback">Insira a senha.</div>
+  				</div>
+  				<button type="submit" class="btn btn-primary">Logar</button>
+			</form>
+
+
+   			</i>
+		</div>
+    
+
+    
+   
 </nav> 
 <img src="images/ante-menu.jpg"></a>
+
+<body>
+  
+</body>
+
 </body>
 </html>
